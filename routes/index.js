f43ca43725f115f839e2519222b6b83f5f8bb73d
jayseo <166495613+seojeongju@users.noreply.github.com>
2025-04-17 19:18:02 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html');
+  res.send('정주의 Node.js 서버입니다!');
 });
-
-module.exports = router;
